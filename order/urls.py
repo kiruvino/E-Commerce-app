664fe2ccf6c502504d2938714c1c8b9ccd6a5dc6
= <=>
2023-12-05 08:15:44 +0530

    Payment details updation in payments table

--- order/urls.py
+++ order/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     
     path('place_order',views.place_order,name='place_order'),
+    path('payments/', views.payments, name='payments'),
     
 ] 
--- order/views.py
+++ order/views.py
@@ -1,10 +1,53 @@
+
+import json
+from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from carts.models import CartItem
-from .models import Order
+from .models import Order,payment,OrderProduct
 from .forms import OrderForm
 import datetime
+from django.core.exceptions import MultipleObjectsReturned
+
 
 # Create your views here.
+def payments(request):
+    body = json.loads(request.body)
+
+    order = Order.objects.all().filter(user=request.user,is_ordered=False).last()
+
+    paymentobject = payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status']
+    )
+
+    paymentobject.save()
+
+    order.payment = paymentobject
+    order.is_ordered = True
+    order.save()
+
+    #Move the cart items to Order product table
+
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id 
+        orderproduct.payment = order.paymentobject 
+        orderproduct.user_id = request.user
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity   
+        orderproduct.product_price = item.product.price 
+        orderproduct.ordered = True
+        orderproduct.save()
+
+        
+
+
+
 
 def place_order(request):
     current_user = request.user
@@ -23,54 +66,51 @@ def place_order(request):
             
     tax = (2 * total)/100
     grand_total = total + tax
-    
+    print(request.method)
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        
-        if form.is_valid():
-            data = Order()
-            data.user = current_user
-            data.first_name = form.cleaned_data['first_name']
-            data.last_name = form.cleaned_data['last_name']
-            data.email = form.cleaned_data['email']
-            data.phone = form.cleaned_data['phone']
-            data.address_line_1 = form.cleaned_data['address_line_1']
-            data.address_line_2 = form.cleaned_data['address_line_2']
-            data.city = form.cleaned_data['city']
-            data.state = form.cleaned_data['state']
-            data.country = form.cleaned_data['country']
-            data.order_note = form.cleaned_data['order_note']
-            data.order_total = grand_total
-            data.tax = tax
-            data.ip = request.META.get('REMOTE_ADDR')
-            data.save()
-
-            #order number generation
-            yr = int(datetime.date.today().strftime('%Y'))
-            dt = int(datetime.date.today().strftime('%d'))
-            mt = int(datetime.date.today().strftime('%m'))
-
-            d = datetime.date(yr,mt,dt)
-            current_date = d.strftime('%Y%m%d')
-            data.order_number = current_date + str(data.id)
-            data.save()
-
-            order = Order.objects.get(user = current_user,is_ordered = False)
-            context = {
-                 'order':order,
-                 'cart_items':cart_items,
-                 'total':total,
-                 'tax':tax,
-                 'grand_total':grand_total
-            }
-
-            return render(request,'payments.html',context)
-        else:
-             return redirect('checkout')
-        
-     
-   
+        try:
+            form = OrderForm(request.POST)
             
+            if form.is_valid():
+                data = Order()
+                data.user = current_user
+                data.first_name = form.cleaned_data['first_name']
+                data.last_name = form.cleaned_data['last_name']
+                data.email = form.cleaned_data['email']
+                data.phone = form.cleaned_data['phone']
+                data.address_line_1 = form.cleaned_data['address_line_1']
+                data.address_line_2 = form.cleaned_data['address_line_2']
+                data.city = form.cleaned_data['city']
+                data.state = form.cleaned_data['state']
+                data.country = form.cleaned_data['country']
+                data.order_note = form.cleaned_data['order_note']
+                data.order_total = grand_total
+                data.tax = tax
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.save()
 
+                #order number generation
+                yr = int(datetime.date.today().strftime('%Y'))
+                dt = int(datetime.date.today().strftime('%d'))
+                mt = int(datetime.date.today().strftime('%m'))
 
+                d = datetime.date(yr,mt,dt)
+                current_date = d.strftime('%Y%m%d')
+                data.order_number = current_date + str(data.id)
+                data.save()
+                
+                order = Order.objects.all().filter(user = current_user,is_ordered = False).last()
+                
+                context = {
+                    'order':order,
+                    'cart_items':cart_items,
+                    'total':total,
+                    'tax':tax,
+                    'grand_total':grand_total
+                }
 
+                return render(request,'payments.html',context)
+            else:
+             return redirect('checkout')
+        except MultipleObjectsReturned:
+            pass

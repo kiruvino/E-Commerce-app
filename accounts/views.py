e9fe2bef266c649c6b8e7b5923bdc69f6592e6eb
= <=>
2023-12-19 08:09:44 +0530

    change password code

--- accounts/views.py
+++ accounts/views.py
@@ -34,6 +34,12 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            #create a user profile
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = 'default/default-user.png'
+            profile.save()
             #logic for sending email to verify email address
             current_site  = get_current_site(request)
             print(current_site)
@@ -151,8 +157,11 @@ def activate(request,uidb64,token):
 def dashboard(request):
      orders = Order.objects.filter(user=request.user,is_ordered=True).order_by('-created_at')
      orders_count = orders.count()
+     userprofile = UserProfile.objects.get(user_id=request.user.id)
+
      context = {
-         'orders_count':orders_count
+         'orders_count':orders_count,
+         'userprofile':userprofile
      }
      return render(request,'accounts/dashboard.html',context)
 
@@ -179,6 +188,7 @@ def order_detail(request, order_id):
     }
     return render(request, 'order_detail.html', context)
 
+@login_required(login_url='login')
 def edit_profile(request):
     user_profile = None
     try:
@@ -199,7 +209,35 @@ def edit_profile(request):
          profile_form = UserProfileForm(instance=user_profile)
     context ={
              'user_form': user_form,
-             'profile_form':profile_form
+             'profile_form':profile_form,
+             'user_profile':user_profile
          }
     return render(request, 'accounts/edit_profile.html', context)
+
+@login_required(login_url='login')
+def change_password(request):
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+        user =   Account.objects.get(username__iexact=request.user.username)
+        if new_password == confirm_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+                auth.logout(request)
+                
+            else:
+                messages.error(request, "please enter valid current password")
+                
+        else:
+            messages.error(request, "password does not match")
+            
+
+
+    return render(request, 'accounts/change_password.html')
+
+
+
 
